4282764c7a03f4f36c6ecb58efeca9cdde48868a
mbvasquez-admin <mbvasquez@crimson.ua.edu>
2026-02-10 23:41:46 -0600

    added project-1 files

---
+++ Spring2026-Project-1-mbvasquez/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spring2026_Project_1_mbvasquez.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
